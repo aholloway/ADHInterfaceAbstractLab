a26c2ca181909e7b65f7046471071d492f361c58
Brian <Brian@192.168.1.123>
2012-09-12 22:31:47 -0500

    Getting tired.  Created some logic in the StartUp class to ask user questions about which class they would like to take.  Will need to create a routine to instantiate methods.

--- src/lab1/StartUp.java
+++ src/lab1/StartUp.java
@@ -5,15 +5,31 @@ package lab1;
  * @Drew Holloway
  */
 public class StartUp {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         //instantiate GUI
-        Gui gui=new Gui();
-        
-        String answer=gui.askUser("How are you?");
-        
-        gui.showMessage("You said you were "+answer);
-        
-        gui.displayErrorMessage(answer);
+        Gui gui = new Gui();
+
+        String answer;
+
+        //answer=gui.askUser("How are you?");
+
+        //gui.showMessage("You said you were "+answer);
+
+        //gui.displayErrorMessage(answer);
+
+        answer = gui.askYesNo("Would you like to take Intro to Programming?");
+        if (answer == "yes") {
+            // take Intro to Programming Class
+        } else {
+            answer = gui.askYesNo("Would you like to take Intro to Java?");
+            if (answer == "yes") {
+                // take Intro to Java Class
+            } else {
+                gui.showMessage("Then you must be hear for Advanced Java!!!");
+            }
+        }
+
     }
 }
